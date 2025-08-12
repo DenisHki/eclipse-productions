9b96892b531d0437cc41f1ac840ac11d8d3d6a31
Denis Chuvakov <ds.chuvakov@gmail.com>
2025-08-12 19:29:20 +0300

    Remove scroll restoration override from App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,8 @@ import Feature from "./components/Services";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Music from "./components/Music";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    if ("scrollRestoration" in window.history) {
-      window.history.scrollRestoration = "manual";
-    }
-
-    const timeoutId = setTimeout(() => {
-      window.scrollTo(0, 0);
-
-      if (window.location.hash) {
-        history.replaceState(
-          null,
-          "",
-          window.location.pathname + window.location.search
-        );
-      }
-    }, 0);
-
-    return () => clearTimeout(timeoutId);
-  }, []);
-
   return (
     <main className="font-bodyFont w-full h-auto bg-bodyColor text-lightText">
       <Navbar />
