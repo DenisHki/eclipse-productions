f19f4752e456da63c0d2cb6df7af62fc1600eca1
Denis Chuvakov <ds.chuvakov@gmail.com>
2025-09-25 14:33:34 +0300

    Add navigation from Booking component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
 import { logo } from "../assets";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <header className="w-full h-24 flex items-center justify-start px-8 bg-black mb-8">
-      <img src={logo} alt="Eclipse Productions Oy" className="h-24 w-auto" />
+      <img
+        onClick={() => navigate("/")}
+        title="Return to home page"
+        src={logo}
+        alt="Eclipse Productions Oy"
+        className="h-24 w-auto"
+      />
     </header>
   );
 }
