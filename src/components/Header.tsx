9892807fd35138929dbcfd5be98d51e2871f999e
Denis Chuvakov <ds.chuvakov@gmail.com>
2025-09-27 23:41:09 +0300

    Adjust headers logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,13 @@ export default function Header() {
   const navigate = useNavigate();
 
   return (
-    <header className="w-full h-24 flex items-center justify-start px-8 bg-black mb-8">
+    <header className="w-full h-26 flex items-center justify-start px-4 bg-black mb-8">
       <img
         onClick={() => navigate("/")}
         title="Return to home page"
         src={logo}
         alt="Eclipse Productions Oy"
-        className="h-24 w-auto"
+        className="h-26 w-auto"
       />
     </header>
   );
