7ffc3fe70c2ca85db480f9d6a43b89be58b98b0b
Denis Chuvakov <ds.chuvakov@gmail.com>
2026-03-23 11:18:50 +0200

    add new media

--- src/playList.ts
+++ src/playList.ts
@@ -48,4 +48,11 @@ export const playList = [
     src: "https://denishki.github.io/eclipse-audio-host/groundbreaker_release.wav",
     id: 1,
   },
+  {
+    name: "FallingDeeper",
+    writer: "Charkki X Reino Ray",
+    img: "https://denishki.github.io/eclipse-audio-host/falling_deeper_original.png",
+    src: "https://denishki.github.io/eclipse-audio-host/falling_deeper.wav",
+    id: 2,
+  },
 ];
