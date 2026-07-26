990d64f6633460856d92474d5313836f4bffc5a3
Denis Chuvakov <ds.chuvakov@gmail.com>
2026-07-26 11:15:17 +0300

    Change playlist order

--- src/playList.ts
+++ src/playList.ts
@@ -1,16 +1,16 @@
 export const playList = [
   {
-    name: "Groundbreaker",
-    writer: "Reino Ray",
-    img: "https://denishki.github.io/eclipse-audio-host/groundbreaker_original.png",
-    src: "https://denishki.github.io/eclipse-audio-host/groundbreaker_release.wav",
-    id: 1,
-  },
-  {
     name: "FallingDeeper",
     writer: "Charkki X Reino Ray",
     img: "https://denishki.github.io/eclipse-audio-host/falling_deeper_original.png",
     src: "https://denishki.github.io/eclipse-audio-host/falling_deeper.wav",
+    id: 1,
+  },
+  {
+    name: "Groundbreaker",
+    writer: "Reino Ray",
+    img: "https://denishki.github.io/eclipse-audio-host/groundbreaker_original.png",
+    src: "https://denishki.github.io/eclipse-audio-host/groundbreaker_release.wav",
     id: 2,
   },
 ];
